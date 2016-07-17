6d562cf472ed60b12e5a819e3dfb31485fdd245e
Nicholas Lindsay Wilson <iamthewilsonator@hotmail.com>
2016-07-17 15:00:04 +0800

    opencl indicies

--- source/dcompute/std/opencl/index.d
+++ source/dcompute/std/opencl/index.d
@@ -1,18 +1,23 @@
 @compute module dcompute.std.opencl.index;
 
 import dcompute.attributes;
+extern(C):
+size_t get_work_dim(uint);
+size_t get_global_size(uint);
+size_t get_global_id(uint);
+size_t get_global_offset(uint);
+size_t get_local_size(uint);
+size_t get_enqueued_local_size(uint);
+size_t get_local_id(uint);
+size_t get_num_groups();
+size_t get_group_id(uint);
+size_t get_global_linear_id(uint);
+size_t get_local_linear_id(uint);
+size_t get_sub_group_size(uint);
+size_t get_max_sub_group_size(uint);
+size_t get_num_sub_groups();
+size_t get_enqueued_num_sub_groups();
+size_t get_sub_group_id(uint);
+size_t get_sub_group_local_id(uint);
 
-extern(C) ulong get_num_groups(uint);
-
-extern(C) ulong get_local_size(uint);
-
-extern(C) ulong get_group_id(uint);
-
-extern(C) ulong get_local_id(uint);
-
-extern(C) ulong get_global_size(uint);
-
-extern(C) ulong get_global_id(uint);
-
-extern(C) ulong get_global_offset(uint);
 
