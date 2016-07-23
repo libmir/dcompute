271145b36bd6aebfdbda583d2749333c8de1f972
Nicholas Lindsay Wilson <iamthewilsonator@hotmail.com>
2016-07-23 09:54:21 +0800

    Update read me

--- source/dcompute/std/opencl/index.d
+++ source/dcompute/std/opencl/index.d
@@ -1,7 +1,7 @@
 @compute module dcompute.std.opencl.index;
 
 import dcompute.attributes;
-extern(C):
+extern(C++):
 size_t get_work_dim(uint);
 size_t get_global_size(uint);
 size_t get_global_id(uint);
@@ -11,13 +11,14 @@ size_t get_enqueued_local_size(uint);
 size_t get_local_id(uint);
 size_t get_num_groups(uint);
 size_t get_group_id(uint);
-size_t get_global_linear_id(uint);
-size_t get_local_linear_id(uint);
+size_t get_global_linear_id();
+size_t get_local_linear_id();
 size_t get_sub_group_size(uint);
 size_t get_max_sub_group_size(uint);
 size_t get_num_sub_groups();
 size_t get_enqueued_num_sub_groups();
 size_t get_sub_group_id(uint);
 size_t get_sub_group_local_id(uint);
+
 
 
