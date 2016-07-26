73fbdb32a3039380ecc29385a22c95d9d934d0d5
Nicholas Lindsay Wilson <iamthewilsonator@hotmail.com>
2016-07-26 17:02:25 +0800

    Add missing import

--- source/dcompute/tests/dummykernels.d
+++ source/dcompute/tests/dummykernels.d
@@ -1,10 +1,12 @@
 @compute module dcompute.dummykernels;
+
 import dcompute.attributes;
 import dcompute.types.pointer;
 import dcompute.reflect;
 import dcompute.std.index;
+
 @kernel void lfgsf( Pointer!(1,float) a,  Pointer!(1,float) b, float c )
 {
-    auto i = LocalIndex.x;
+    auto i = GlobalIndex.x;
     a[i] = b[i] +c;
 }
