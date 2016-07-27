ffa61047904d77fe87b5a0fa6c55fe610781d44e
Nicholas Lindsay Wilson <iamthewilsonator@hotmail.com>
2016-07-27 19:48:14 +0800

    update the version to ldc agnostic and add a more informative error message

--- source/dcompute/std/package.d
+++ source/dcompute/std/package.d
@@ -1,7 +1,11 @@
 module dcompute.std;
 
-version(LDC_DCompute) {}
-else {static assert(false,"Need to use a DCompute enabled compiler");}
+version(D_DCompute) {}
+else
+{
+    static assert(false, "Need to use a DCompute enabled compiler\n" ~
+                         "See https://github.com/thewilsonator/ldc/tree/dcompute");
+}
 
 
 public import dcompute.std.index;
