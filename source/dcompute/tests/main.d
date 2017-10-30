97bfdb949c60ac84db7f09c86fe7bf6c551b60b9
Nicholas Wilson <thewilsonator@users.noreply.github.com>
2017-10-30 12:39:25 +0800

    tabs to spaces

--- source/dcompute/tests/main.d
+++ source/dcompute/tests/main.d
@@ -31,73 +31,74 @@ int main(string[] args)
         y[i] = i * i;
     }
 
-	version(DComputeTestOpenCL)
-	{
+    version(DComputeTestOpenCL)
+    {
         Platform.initialise();
         onDriverError = (Status _status) { throw new DComputeDriverException(_status); };
-		auto platforms = Platform.getPlatforms(theAllocator);
+        auto platforms = Platform.getPlatforms(theAllocator);
         auto platform = platforms[2];
         DerelictCL.reload(CLVersion.CL21);
         writeln("Platforms:");
         writeln("\t", platforms.map!(p => p.name));
         writeln("\tChosen: ", platform.name);
-		auto devices  = platform.getDevices(theAllocator);
+        auto devices  = platform.getDevices(theAllocator);
         writeln("Devices:");
         writeln("\t", devices.map!(d => d.name));
         writeln("\tChosen: ", devices[0].name);
-		auto plist    = propertyList!(Context.Properties)(Context.Properties.platform, platform.raw);
+        auto plist    = propertyList!(Context.Properties)(Context.Properties.platform, platform.raw);
         writeln(plist);
-		auto ctx      = Context(devices[0 ..1],null /*FIXME: plist[]*/);
+        auto ctx      = Context(devices[0 ..1],null /*FIXME: plist[]*/);
         Program.globalProgram = ctx.createProgram(cast(ubyte[])read("./.dub/obj/kernels_ocl200_64.spv"));
 
         try
         {
             Program.globalProgram.build(devices,"");
         }
-		catch(DComputeDriverException e)
+            catch(DComputeDriverException e)
         {
             auto b = Build(Program.globalProgram, devices[0]);
             writeln(b.log);
         }
         
-		auto queue    = ctx.createQueue(devices[0],Queue.Properties.outOfOrderExecution);
+        auto queue    = ctx.createQueue(devices[0],Queue.Properties.outOfOrderExecution);
 
-		Buffer!(float) b_res, b_x, b_y;
+        Buffer!(float) b_res, b_x, b_y;
 
-		b_res = ctx.createBuffer(res[], Memory.Flags.useHostPointer | Memory.Flags.readWrite);
-		b_x = ctx.createBuffer(x[],Memory.Flags.useHostPointer | Memory.Flags.readWrite);
-		b_y = ctx.createBuffer(y[],Memory.Flags.useHostPointer | Memory.Flags.readWrite);
+        b_res = ctx.createBuffer(res[], Memory.Flags.useHostPointer | Memory.Flags.readWrite);
+        b_x = ctx.createBuffer(x[],Memory.Flags.useHostPointer | Memory.Flags.readWrite);
+        b_y = ctx.createBuffer(y[],Memory.Flags.useHostPointer | Memory.Flags.readWrite);
 
-		Event e = queue.enqueue!(saxpy)([N])(b_res,alpha,b_x,b_y, N);
+        Event e = queue.enqueue!(saxpy)([N])(b_res,alpha,b_x,b_y, N);
         
-		e.wait();
-	}
-	version(DComputeTestCUDA)
-	{
-		Platform.initialise();
+        e.wait();
+    }
+
+    version(DComputeTestCUDA)
+    {
+        Platform.initialise();
 		
-		auto devs	= Platform.getDevices(theAllocator);
-		auto dev	= devs[0]; 
-		auto ctx	= Context(dev); scope(exit) ctx.detach();
+        auto devs = Platform.getDevices(theAllocator);
+        auto dev   = devs[0]; 
+        auto ctx   = Context(dev); scope(exit) ctx.detach();
 
         // Change the file to match your GPU.
         Program.globalProgram = Program.fromFile("./.dub/obj/kernels_cuda210_64.ptx");
         auto q = Queue(false);
 
-		Buffer!(float) b_res, b_x, b_y;
-		b_res	=  Buffer!(float)(res[]); scope(exit) b_res.release();
-		b_x		=  Buffer!(float)(x[]);   scope(exit) b_x.release();
-		b_y		=  Buffer!(float)(y[]);   scope(exit) b_y.release();
+        Buffer!(float) b_res, b_x, b_y;
+        b_res =  Buffer!(float)(res[]); scope(exit) b_res.release();
+        b_x   =  Buffer!(float)(x[]);   scope(exit) b_x.release();
+        b_y   =  Buffer!(float)(y[]);   scope(exit) b_y.release();
 
         b_x.copy!(Copy.hostToDevice);
         b_y.copy!(Copy.hostToDevice);
 
-		q.enqueue!(saxpy)
-			([N,1,1],[1,1,1])
-			(b_res,alpha,b_x,b_y, N);
-		b_res.copy!(Copy.deviceToHost);
-		
-	}
+        q.enqueue!(saxpy)
+                  ([N,1,1],[1,1,1])
+                  (b_res,alpha,b_x,b_y, N);
+        b_res.copy!(Copy.deviceToHost);
+	
+    }
 
     foreach(i; 0 .. N)
         enforce(res[i] == alpha * x[i] + y[i]);
