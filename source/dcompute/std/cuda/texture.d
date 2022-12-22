24723cc3f782618cd2a8018b4ea9afaa3f62bbf6
aferust <aferust@gmail.com>
2022-12-22 14:26:03 +0300

    CUDA texture sampling return types
    CUDA texture sampling return types

---
+++ source/dcompute/std/cuda/texture.d
@@ -0,0 +1,236 @@
+module dcompute.std.cuda.texture;
+
+// CUDA texture sampling return types
+struct int4
+{
+    int x, y, z, w;
+}
+
+struct float4
+{
+    float x, y, z, w;
+
+    float4 opBinary(string op)(float s) if (op == "+"){
+        return float4(x+s, y+s, z+s, w+s);
+    }
+    float4 opBinary(string op)(float s) if (op == "*") {
+        return float4(x*s, y*s, z*s, w*s);
+    }
+    float4 opBinary(string op)(float s) if (op == "/") {
+        return float4(x/s, y/s, z/s, w/s);
+    }
+
+    float4 opBinary(string op)(float4 other){
+        static if (op == "+"){
+            return float4(x+other.x, y+other.y, z+other.z, w+other.w);
+        } else
+            static assert(0, "op is not implemented");
+    }
+}
+
+/++ 
+Sampling a 1D texture:
+
+%float4 @llvm.nvvm.tex.unified.1d.v4f32.s32(i64 %tex, i32 %x)
+%float4 @llvm.nvvm.tex.unified.1d.v4f32.f32(i64 %tex, float %x)
+%float4 @llvm.nvvm.tex.unified.1d.level.v4f32.f32(i64 %tex, float %x,
+                                                  float %level)
+%float4 @llvm.nvvm.tex.unified.1d.grad.v4f32.f32(i64 %tex, float %x,
+                                                 float %dPdx,
+                                                 float %dPdy)
+
+%int4 @llvm.nvvm.tex.unified.1d.v4s32.s32(i64 %tex, i32 %x)
+%int4 @llvm.nvvm.tex.unified.1d.v4s32.f32(i64 %tex, float %x)
+%int4 @llvm.nvvm.tex.unified.1d.level.v4s32.f32(i64 %tex, float %x,
+                                                float %level)
+%int4 @llvm.nvvm.tex.unified.1d.grad.v4s32.f32(i64 %tex, float %x,
+                                               float %dPdx,
+                                               float %dPdy)
+
+%int4 @llvm.nvvm.tex.unified.1d.v4u32.s32(i64 %tex, i32 %x)
+%int4 @llvm.nvvm.tex.unified.1d.v4u32.f32(i64 %tex, float %x)
+%int4 @llvm.nvvm.tex.unified.1d.level.v4u32.f32(i64 %tex, float %x,
+                                                float %level)
+%int4 @llvm.nvvm.tex.unified.1d.grad.v4u32.f32(i64 %tex, float %x,
+                                               float %dPdx,
+                                               float %dPdy)
+
+
+Sampling a 1D texture array:
+
+%float4 @llvm.nvvm.tex.unified.1d.array.v4f32.s32(i64 %tex, i32 %idx, i32 %x)
+%float4 @llvm.nvvm.tex.unified.1d.array.v4f32.f32(i64 %tex, i32 %idx, float %x)
+%float4 @llvm.nvvm.tex.unified.1d.array.level.v4f32.f32(i64 %tex, i32 %idx,
+                                                        float %x,
+                                                        float %level)
+%float4 @llvm.nvvm.tex.unified.1d.array.grad.v4f32.f32(i64 %tex, i32 %idx,
+                                                       float %x,
+                                                       float %dPdx,
+                                                       float %dPdy)
+
+%int4 @llvm.nvvm.tex.unified.1d.array.v4s32.s32(i64 %tex, i32 %idx, i32 %x)
+%int4 @llvm.nvvm.tex.unified.1d.array.v4s32.f32(i64 %tex, i32 %idx, float %x)
+%int4 @llvm.nvvm.tex.unified.1d.array.level.v4s32.f32(i64 %tex, i32 %idx,
+                                                      float %x,
+                                                      float %level)
+%int4 @llvm.nvvm.tex.unified.1d.array.grad.v4s32.f32(i64 %tex, i32 %idx,
+                                                     float %x,
+                                                     float %dPdx,
+                                                     float %dPdy)
+
+%int4 @llvm.nvvm.tex.unified.1d.array.v4u32.s32(i64 %tex, i32 %idx, i32 %x)
+%int4 @llvm.nvvm.tex.unified.1d.array.v4u32.f32(i64 %tex, i32 %idx, float %x)
+%int4 @llvm.nvvm.tex.unified.1d.array.level.v4u32.f32(i64 %tex, i32 %idx,
+                                                      float %x,
+                                                      float %level)
+%int4 @llvm.nvvm.tex.unified.1d.array.grad.v4u32.f32(i64 %tex, i32 %idx,
+                                                     float %x,
+                                                     float %dPdx,
+                                                     float %dPdy)
+
+
+Sampling a 2D texture:
+
+%float4 @llvm.nvvm.tex.unified.2d.v4f32.s32(i64 %tex, i32 %x, i32 %y)
+%float4 @llvm.nvvm.tex.unified.2d.v4f32.f32(i64 %tex, float %x, float %y)
+%float4 @llvm.nvvm.tex.unified.2d.level.v4f32.f32(i64 %tex, float %x, float %y,
+                                                  float %level)
+%float4 @llvm.nvvm.tex.unified.2d.grad.v4f32.f32(i64 %tex, float %x, float %y,
+                                                 float %dPdx_x, float %dPdx_y,
+                                                 float %dPdy_x, float %dPdy_y)
+
+%int4 @llvm.nvvm.tex.unified.2d.v4s32.s32(i64 %tex, i32 %x, i32 %y)
+%int4 @llvm.nvvm.tex.unified.2d.v4s32.f32(i64 %tex, float %x, float %y,)
+%int4 @llvm.nvvm.tex.unified.2d.level.v4s32.f32(i64 %tex, float %x, float %y,
+                                                float %level)
+%int4 @llvm.nvvm.tex.unified.2d.grad.v4s32.f32(i64 %tex, float %x, float %y,
+                                               float %dPdx_x, float %dPdx_y,
+                                               float %dPdy_x, float %dPdy_y)
+
+%int4 @llvm.nvvm.tex.unified.2d.v4u32.s32(i64 %tex, i32 %x i32 %y)
+%int4 @llvm.nvvm.tex.unified.2d.v4u32.f32(i64 %tex, float %x float %y)
+%int4 @llvm.nvvm.tex.unified.2d.level.v4u32.f32(i64 %tex, float %x, float %y,
+                                                float %level)
+%int4 @llvm.nvvm.tex.unified.2d.grad.v4u32.f32(i64 %tex, float %x, float %y,
+                                               float %dPdx_x, float %dPdx_y,
+                                               float %dPdy_x, float %dPdy_y)
+
+Sampling a 2D texture array:
+
+%float4 @llvm.nvvm.tex.unified.2d.array.v4f32.s32(i64 %tex, i32 %idx,
+                                                  i32 %x, i32 %y)
+%float4 @llvm.nvvm.tex.unified.2d.array.v4f32.f32(i64 %tex, i32 %idx,
+                                                  float %x, float %y)
+%float4 @llvm.nvvm.tex.unified.2d.array.level.v4f32.f32(i64 %tex, i32 %idx,
+                                                        float %x, float %y,
+                                                        float %level)
+%float4 @llvm.nvvm.tex.unified.2d.array.grad.v4f32.f32(i64 %tex, i32 %idx,
+                                                       float %x, float %y,
+                                                       float %dPdx_x,
+                                                       float %dPdx_y,
+                                                       float %dPdy_x,
+                                                       float %dPdy_y)
+
+%int4 @llvm.nvvm.tex.unified.2d.array.v4s32.s32(i64 %tex, i32 %idx,
+                                                i32 %x, i32 %y)
+%int4 @llvm.nvvm.tex.unified.2d.array.v4s32.f32(i64 %tex, i32 %idx,
+                                                float %x, float %y)
+%int4 @llvm.nvvm.tex.unified.2d.array.level.v4s32.f32(i64 %tex, i32 %idx,
+                                                      float %x, float %y,
+                                                      float %level)
+%int4 @llvm.nvvm.tex.unified.2d.array.grad.v4s32.f32(i64 %tex, i32 %idx,
+                                                     float %x, float %y,
+                                                     float %dPdx_x,
+                                                     float %dPdx_y,
+                                                     float %dPdy_x,
+                                                     float %dPdy_y)
+
+%int4 @llvm.nvvm.tex.unified.2d.array.v4u32.s32(i64 %tex, i32 %idx,
+                                                i32 %x i32 %y)
+%int4 @llvm.nvvm.tex.unified.2d.array.v4u32.f32(i64 %tex, i32 %idx,
+                                                float %x float %y)
+%int4 @llvm.nvvm.tex.unified.2d.array.level.v4u32.f32(i64 %tex, i32 %idx,
+                                                      float %x, float %y,
+                                                      float %level)
+%int4 @llvm.nvvm.tex.unified.2d.array.grad.v4u32.f32(i64 %tex, i32 %idx,
+                                                     float %x, float %y,
+                                                     float %dPdx_x,
+                                                     float %dPdx_y,
+                                                     float %dPdy_x,
+                                                     float %dPdy_y)
+
+Sampling a 3D texture:
+
+%float4 @llvm.nvvm.tex.unified.3d.v4f32.s32(i64 %tex, i32 %x, i32 %y, i32 %z)
+%float4 @llvm.nvvm.tex.unified.3d.v4f32.f32(i64 %tex, float %x, float %y,
+                                            float %z)
+%float4 @llvm.nvvm.tex.unified.3d.level.v4f32.f32(i64 %tex,float %x, float %y,
+                                                  float %z, float %level)
+%float4 @llvm.nvvm.tex.unified.3d.grad.v4f32.f32(i64 %tex, float %x, float %y,
+                                                 float %z, float %dPdx_x,
+                                                 float %dPdx_y, float %dPdx_z,
+                                                 float %dPdy_x, float %dPdy_y,
+                                                 float %dPdy_z)
+
+%int4 @llvm.nvvm.tex.unified.3d.v4s32.s32(i64 %tex, i32 %x, i32 %y, i32 %z)
+%int4 @llvm.nvvm.tex.unified.3d.v4s32.f32(i64 %tex, float %x, float %y,
+                                          float %z)
+%int4 @llvm.nvvm.tex.unified.3d.level.v4s32.f32(i64 %tex, float %x, float %y,
+                                                float %z, float %level)
+%int4 @llvm.nvvm.tex.unified.3d.grad.v4s32.f32(i64 %tex, float %x, float %y,
+                                               float %z, float %dPdx_x,
+                                               float %dPdx_y, float %dPdx_z,
+                                               float %dPdy_x, float %dPdy_y,
+                                               float %dPdy_z)
+
+%int4 @llvm.nvvm.tex.unified.3d.v4u32.s32(i64 %tex, i32 %x i32 %y, i32 %z)
+%int4 @llvm.nvvm.tex.unified.3d.v4u32.f32(i64 %tex, float %x, float %y,
+                                          float %z)
+%int4 @llvm.nvvm.tex.unified.3d.level.v4u32.f32(i64 %tex, float %x, float %y,
+                                                float %z, float %level)
+%int4 @llvm.nvvm.tex.unified.3d.grad.v4u32.f32(i64 %tex, float %x, float %y,
+                                               float %z, float %dPdx_x,
+                                               float %dPdx_y, float %dPdx_z,
+                                               float %dPdy_x, float %dPdy_y,
+                                               float %dPdy_z)
+
+Sampling a cube texture:
+
+%float4 @llvm.nvvm.tex.unified.cube.v4f32.f32(i64 %tex, float %x, float %y,
+                                              float %z)
+%float4 @llvm.nvvm.tex.unified.cube.level.v4f32.f32(i64 %tex,float %x, float %y,
+                                                    float %z, float %level)
+
+%int4 @llvm.nvvm.tex.unified.cube.v4s32.f32(i64 %tex, float %x, float %y,
+                                            float %z)
+%int4 @llvm.nvvm.tex.unified.cube.level.v4s32.f32(i64 %tex, float %x, float %y,
+                                                  float %z, float %level)
+
+%int4 @llvm.nvvm.tex.unified.cube.v4u32.f32(i64 %tex, float %x, float %y,
+                                            float %z)
+%int4 @llvm.nvvm.tex.unified.cube.level.v4u32.f32(i64 %tex, float %x, float %y,
+                                                  float %z, float %level)
+
+Sampling a cube texture array:
+
+%float4 @llvm.nvvm.tex.unified.cube.array.v4f32.f32(i64 %tex, i32 %idx,
+                                                    float %x, float %y,
+                                                    float %z)
+%float4 @llvm.nvvm.tex.unified.cube.array.level.v4f32.f32(i64 %tex, i32 %idx,
+                                                          float %x, float %y,
+                                                          float %z,
+                                                          float %level)
+
+%int4 @llvm.nvvm.tex.unified.cube.array.v4s32.f32(i64 %tex, i32 %idx, float %x,
+                                                  float %y, float %z)
+%int4 @llvm.nvvm.tex.unified.cube.array.level.v4s32.f32(i64 %tex, i32 %idx,
+                                                        float %x, float %y,
+                                                        float %z, float %level)
+
+%int4 @llvm.nvvm.tex.unified.cube.array.v4u32.f32(i64 %tex, i32 %idx, float %x,
+                                                  float %y, float %z)
+%int4 @llvm.nvvm.tex.unified.cube.array.level.v4u32.f32(i64 %tex, i32 %idx,
+                                                        float %x, float %y,
+                                                        float %z, float %level)
+
+ +/
