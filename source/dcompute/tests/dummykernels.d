231585b458d38d943d06fb3882d97bf1932b7fb4
Yutaka Tamura <ta.kinutani@gmail.com>
2026-03-08 14:08:18 +0900

    recover dummykernels.d, with supports only >=LDC 1.42.0

--- source/dcompute/tests/dummykernels.d
+++ source/dcompute/tests/dummykernels.d
@@ -3,38 +3,23 @@ module dcompute.tests.dummykernels;
 pragma(LDC_no_moduleinfo);
 
 import ldc.dcompute;
-import ldc.intrinsics;
 import dcompute.std.index;
 
-mixin template _saxpy() {
-    void saxpy(GlobalPointer!(float) res,
-                    float alpha,GlobalPointer!(float) x,
-                    GlobalPointer!(float) y, 
-                    size_t N)
-    {
-        auto i = GlobalIndex.x;
-        if (i >= N) return;
-        res[i] = alpha*x[i] + y[i];
-    }    
+@kernel() void saxpy(GlobalPointer!(float) res,
+                   float alpha,GlobalPointer!(float) x,
+                   GlobalPointer!(float) y, 
+                   size_t N)
+{
+    auto i = GlobalIndex.x;
+    if (i >= N) return;
+    res[i] = alpha*x[i] + y[i];
 }
 
 alias aagf = AutoIndexed!(GlobalPointer!(float));
 
-mixin template _auto_index_test() {
-    void auto_index_test(aagf a,
-                            aagf b,
-                            aagf c)
-    {
-        a = b + c;
-    }
-}
-
-static if (LLVM_atleast!21) { // >= LDC 1.42.0(LLVM 21)
-    pragma(msg, "using LDC version >= 1.42.0(LLVM 21)");
-    @kernel() mixin _saxpy;
-    @kernel() mixin _auto_index_test;
-} else {
-    pragma(msg, "using LDC version < 1.42.0(LLVM 21)");
-    @kernel mixin _saxpy;
-    @kernel mixin _auto_index_test;
+@kernel() void auto_index_test(aagf a,
+                             aagf b,
+                             aagf c)
+{
+    a = b + c;
 }
