4b80239ef402248d32412c55f1795d84d069f27b
gulugulubing <413153391@qq.com>
2026-05-10 17:43:31 -0600

    change index style

--- source/dcompute/std/metal/index.d
+++ source/dcompute/std/metal/index.d
@@ -3,32 +3,39 @@
 import ldc.dcompute;
 pure: nothrow: @nogc:
 
+pragma(LDC_intrinsic, "air.get_global_id.i32") uint get_global_id(uint dim);
+pragma(LDC_intrinsic, "air.get_local_id.i32") uint get_local_id(uint dim);
+pragma(LDC_intrinsic, "air.get_group_id.i32") uint get_group_id(uint dim);
+pragma(LDC_intrinsic, "air.get_global_size.i32") uint get_global_size(uint dim);
+pragma(LDC_intrinsic, "air.get_local_size.i32") uint get_local_size(uint dim);
+pragma(LDC_intrinsic, "air.get_num_groups.i32") uint get_num_groups(uint dim);
+
 // thread_position_in_grid → GlobalIndex
-pragma(LDC_intrinsic, "air.thread_position_in_grid.x") uint get_global_id_x();
-pragma(LDC_intrinsic, "air.thread_position_in_grid.y") uint get_global_id_y();
-pragma(LDC_intrinsic, "air.thread_position_in_grid.z") uint get_global_id_z();
+uint get_global_id_x()() { return get_global_id(0); }
+uint get_global_id_y()() { return get_global_id(1); }
+uint get_global_id_z()() { return get_global_id(2); }
 
 // thread_position_in_threadgroup → SharedIndex
-pragma(LDC_intrinsic, "air.thread_position_in_threadgroup.x") uint get_local_id_x();
-pragma(LDC_intrinsic, "air.thread_position_in_threadgroup.y") uint get_local_id_y();
-pragma(LDC_intrinsic, "air.thread_position_in_threadgroup.z") uint get_local_id_z();
+uint get_local_id_x()() { return get_local_id(0); }
+uint get_local_id_y()() { return get_local_id(1); }
+uint get_local_id_z()() { return get_local_id(2); }
 
 // threadgroup_position_in_grid → GroupIndex
-pragma(LDC_intrinsic, "air.threadgroup_position_in_grid.x") uint get_group_id_x();
-pragma(LDC_intrinsic, "air.threadgroup_position_in_grid.y") uint get_group_id_y();
-pragma(LDC_intrinsic, "air.threadgroup_position_in_grid.z") uint get_group_id_z();
+uint get_group_id_x()() { return get_group_id(0); }
+uint get_group_id_y()() { return get_group_id(1); }
+uint get_group_id_z()() { return get_group_id(2); }
 
 // threads_per_grid → GlobalDimension
-pragma(LDC_intrinsic, "air.threads_per_grid.x") uint get_global_size_x();
-pragma(LDC_intrinsic, "air.threads_per_grid.y") uint get_global_size_y();
-pragma(LDC_intrinsic, "air.threads_per_grid.z") uint get_global_size_z();
+uint get_global_size_x()() { return get_global_size(0); }
+uint get_global_size_y()() { return get_global_size(1); }
+uint get_global_size_z()() { return get_global_size(2); }
 
 // threads_per_threadgroup → SharedDimension
-pragma(LDC_intrinsic, "air.threads_per_threadgroup.x") uint get_local_size_x();
-pragma(LDC_intrinsic, "air.threads_per_threadgroup.y") uint get_local_size_y();
-pragma(LDC_intrinsic, "air.threads_per_threadgroup.z") uint get_local_size_z();
+uint get_local_size_x()() { return get_local_size(0); }
+uint get_local_size_y()() { return get_local_size(1); }
+uint get_local_size_z()() { return get_local_size(2); }
 
 // threadgroups_per_grid → GroupDimension
-pragma(LDC_intrinsic, "air.threadgroups_per_grid.x") uint get_num_groups_x();
-pragma(LDC_intrinsic, "air.threadgroups_per_grid.y") uint get_num_groups_y();
-pragma(LDC_intrinsic, "air.threadgroups_per_grid.z") uint get_num_groups_z();
+uint get_num_groups_x()() { return get_num_groups(0); }
+uint get_num_groups_y()() { return get_num_groups(1); }
+uint get_num_groups_z()() { return get_num_groups(2); }
